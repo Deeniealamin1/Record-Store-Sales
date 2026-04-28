5350488bc52c412e039198ccfc884deedadbcf72
Salahuddin Al-Amin <salahuddin.alamin1@gmail.com>
2026-04-28 09:54:35 -0700

    ledger

---
+++ src/main/java/com/pluralsight/Ledger.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ledger {
+    Scanner scanner = new Scanner(System.in);
+    List<Transaction> transactions = new ArrayList<>();
+}
